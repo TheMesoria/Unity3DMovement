8fa5465b18f664f508600aad00ba8b3e034ebf9e
ormaniec <wojciech.ormaniec@gmail.com>
2022-02-16 23:18:47 +0100

    Initial commit

---
+++ Assets/Scripts/ExampleScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleScript : MonoBehaviour
+{
+    // References
+
+    // Customizable
+
+    // Script specific
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
